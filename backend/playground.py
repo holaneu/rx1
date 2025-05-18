b57a418e3b33feabd71fce3852a42618720356ec
holaneu <hello@holan.eu>
2025-05-18 10:11:16 +0200

    upd

--- backend/playground.py
+++ backend/playground.py
@@ -1,5 +1,7 @@
 
-from app import *
-workflow_manager = WorkflowManager()
-wfs = list(workflow_manager.registered_funcs.keys())
-print(wfs)
+from app import workflow_manager  # Import the existing instance instead of creating new one
+
+wfs1 = list(workflow_manager.registered_funcs.keys())
+wfs2 = workflow_manager.get_available_workflows_for_api()
+print(wfs1)
+print(wfs2)
