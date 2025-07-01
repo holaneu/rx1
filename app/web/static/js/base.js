f10a8562d0dc211411c483ed1f69c69fa75fb236
holaneu <hello@holan.eu>
2025-07-01 10:26:42 +0200

    sidebar logic updated

--- app/web/static/js/base.js
+++ app/web/static/js/base.js
@@ -3,44 +3,75 @@ const sidebar = document.getElementById('sidebar');
 const overlay = document.getElementById('overlay');
 const mainContent = document.querySelector('.main-content');
 
-function adjustSidebar() {
-    const windowWidth = window.innerWidth;
-    const contentMax = 800;
-    const sidebarWidth = 230;
-    if (windowWidth >= contentMax + sidebarWidth + 20) {
-    sidebar.classList.add('active');
-    overlay.classList.remove('active');
-    mainContent.style.transform = "translateX(115px)";
-    } else {
-    sidebar.classList.remove('active');
-    overlay.classList.remove('active');
-    mainContent.style.transform = "translateX(0)";
-    }
+const sidebarLsKey = 'sidebarActivated';
+const isSidebarAllowed = function(){
+    return (localStorage.getItem(sidebarLsKey) || 'true') === 'true';
 }
 
 
 
-hamburger.addEventListener('click', () => {
-    const windowWidth = window.innerWidth;
-    const isActive = sidebar.classList.contains('active');
-    if (!isActive) {
-    sidebar.classList.add('active');
-    if (windowWidth < 1024) {
-        overlay.classList.add('active');
-    } else {
-        mainContent.style.transform = "translateX(115px)";
-    }
-    } else {
-    sidebar.classList.remove('active');
-    overlay.classList.remove('active');
+function openSidebar() {
+  sidebar.classList.add('active');  
+  if (window.innerWidth < 1024) {    
+    overlay.classList.add('active');
     mainContent.style.transform = "translateX(0)";
+  } else {
+    overlay.classList.remove('active');
+    mainContent.style.transform = "translateX(115px)";
+  }
+}
+
+function closeSidebar() {  
+  sidebar.classList.remove('active');
+  overlay.classList.remove('active');
+  mainContent.style.transform = "translateX(0)";
+}
+
+function setInitialSidebarState() {
+  const windowWidth = window.innerWidth;
+  const contentMax = 800;
+  const sidebarWidth = 230;
+  if (windowWidth >= contentMax + sidebarWidth + 20 && isSidebarAllowed() ) {
+    openSidebar(); // no transition yet
+  } else {
+    closeSidebar(); // no transition yet
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  setInitialSidebarState();
+  requestAnimationFrame(() => {
+    document.body.classList.add('transition-enabled');
+  });
+});
+
+window.addEventListener('resize', () => {
+  const windowWidth = window.innerWidth;
+  console.log('Window resized to:', windowWidth, '\nSidebar allowed:', isSidebarAllowed() );
+  if (isSidebarAllowed() ) {
+    if (windowWidth >= 1024) {
+        openSidebar();
+    } else {
+        closeSidebar();
     }
+ }
+});
+
+hamburger.addEventListener('click', () => {
+  const windowWidth = window.innerWidth;
+  if (sidebar.classList.contains('active')) {
+    closeSidebar();
+    if (windowWidth >= 1024) {
+        localStorage.setItem(sidebarLsKey, 'false');
+    }
+  } else {
+    openSidebar();
+    if (windowWidth >= 1024) {
+        localStorage.setItem(sidebarLsKey, 'true');
+    }
+  }
 });
 
 overlay.addEventListener('click', () => {
-    sidebar.classList.remove('active');
-    overlay.classList.remove('active');
+  closeSidebar();
 });
-
-window.addEventListener('resize', adjustSidebar);
-window.addEventListener('DOMContentLoaded', adjustSidebar);
